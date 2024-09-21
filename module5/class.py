01f5ede76d19499235e81c3a90698f93735fefbc
Timur <bagmanov.tima@mail.ru>
2024-09-21 13:37:54 +0300

    module 5

--- module5/class.py
+++ module5/class.py
@@ -3,6 +3,7 @@ class Human:
         self.name = name
         self.age = age
         self.say_INFO()
+
     def say_INFO(self):
         print(f'Привет, меня зовут: {self.name}, мне: {self.age}')
 
@@ -10,8 +11,14 @@ class Human:
         self.age += 1
         print(f'Поздравляю с Днем Рождения! Теперь мне: {self.age}')
 
+    def __len__(self):
+        return self.age
+
+    def __del__(self):
+        print(f'{self.name} ушёл')
+
 
 tim = Human('Timur', 31)
 tan = Human("Tanya", 26)
-
-tim.birtheday()
+tan.birtheday()
+print(len(tan))
