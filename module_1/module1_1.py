3902e04eedfd2a4c04d99f9cf38f955f075b9211
Timur <bagmanov.tima@mail.ru>
2024-11-20 13:29:44 +0300

    20/11

--- module_1/module1_1.py
+++ module_1/module1_1.py
@@ -1,5 +1,7 @@
 print(9 ** 0.5 * 5) # 1stProgram
 print(9.99 > 9.98 and 1000 != 1000.1) # 2ndProgram
 print((2 * 2 + 2) == (2 + 2) * 2) #3rd Program
-print(float('123.456') * 10) #4rd Program
-print(int(1234.56 % 10))
+num = float('123.456') #4th Program
+num *= 10
+num_2 = int(num) % 10
+print(num_2)
